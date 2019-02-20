487a5df0a015a7cb61d2c0a8d6d75e8556214423
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-02-20 11:05:55 +0000

    working copy?

--- DatagramPanel.java
+++ DatagramPanel.java
@@ -12,47 +12,21 @@ import java.io.*;
 public class DatagramPanel extends JPanel {
     public static final long serialVersionUID = 2L;
 
-    JTextField address;
-    JTextField port;
+    JTextField addressname, port;
 
     public DatagramPanel() {
-        /* default dummy values */
-        this("192.168.255.254", 65535);
-    }
-
-    public DatagramPanel(String address, int port) {
         /* create a JPanel populated with border and text fields */
         super( new FlowLayout( FlowLayout.LEFT, 5, 0));
-        this.setBorder( BorderFactory.createTitledBorder("Socket Address"));
-        this.add(new JLabel("IP:"));
-        this.address = new JTextField(address);
-        this.add(this.address);
-        this.add(new JLabel("port:"));
-        this.port = new JTextField(Integer.toString(port));
-        this.add(this.port);
+        setBorder( BorderFactory.createTitledBorder("Socket Address"));
+        add(new JLabel("IP:"));
+        addressname = new JTextField(10);
+        add(this.addressname);
+        add(new JLabel("port:"));
+        port = new JTextField(5);
+        add(this.port);
     }
-
-    public void setAddress(String address){
-        this.address.setText(address);
+    void setAddress(InetSocketAddress where){
+        addressname.setText(where.getHostString());
+        port.setText( Integer.toString(where.getPort()) );
     }
-
-    public void setAddress(String address, boolean edit){
-        this.address.setText(address);
-        this.address.setEditable(edit);
-    }
-
-    public void setPort(int port){
-        this.port.setText(Integer.toString(port));
-    }
-
-    public void setPort(int port, boolean edit){
-        this.port.setText(Integer.toString(port));
-        this.port.setEditable(edit);
-    }
-
-    public InetSocketAddress getSocketAddress() {
-        return new InetSocketAddress(address.getText(),
-                        Integer.parseInt(port.getText()));
-    }
-
 }
---
+++ lander/Controller.java
@@ -0,0 +1,65 @@
+package lander;
+import java.util.Arrays;
+public class Controller {
+    Model lander;
+    public Controller(Model model){
+        lander = model;
+    }
+
+    public String handle(String[] message){
+        String reply = "";
+        /* first line of message */
+        String[] type = message[0].split(":");
+        int lines = message.length; /*count lines after first*/
+        String[] payload = Arrays.copyOfRange(message,1,lines);
+        switch( type[0] ){
+            case "command":
+                reply = doCommand(payload);
+            break;
+            case "state":
+                reply = doState(payload);
+            break;
+        }
+        return reply;
+    }
+
+    String doCommand(String[] content) {
+        StringBuffer response = new StringBuffer("command:=\n");
+        for(String line : content){
+            String[] pair = line.split(":");
+            switch( pair[0] ){
+                case "throttle":
+                    lander.throttle = Double.parseDouble( pair[1] );
+                break;
+                case "roll":
+                    lander.roll = Double.parseDouble( pair[1] );
+                break;
+            }
+        }
+        response = response.append("altitude:"+lander.altitude+"\n");
+        response = response.append("fuel:"+lander.fuel+"\n");
+        response = response.append("flying:"+ ((lander.isflying)?1:0) +"\n");
+        response = response.append("crashed:"+ ((lander.iscrashed)?1:0) +"\n");
+        response = response.append("orientation:"+ lander.O +"\n");
+        response = response.append("Vx:"+ lander.xdot +"\n");
+        response = response.append("Vy:"+ lander.ydot +"\n");
+
+        return response.toString();
+    }
+
+    String doState(String[] content) {
+        StringBuffer response = new StringBuffer("state:=\n");
+        for(String line : content){
+            String[] pair = line.split(":");
+            switch( pair[0] ){
+                case "throttle":
+                    lander.throttle = Double.parseDouble( pair[1] );
+                break;
+                case "roll":
+                    lander.roll = Double.parseDouble( pair[1] );
+                break;
+            }
+        }
+        return response.toString();
+    }
+}
